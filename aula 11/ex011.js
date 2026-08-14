2d8b74c2780fd6f9467ea78df346a38f53bdf710
juliano-magalhaes <aj.magalhaes1996@gmail.com>
2026-08-14 15:23:32 -0300

    ex011

---
+++ aula 11/ex011.js
@@ -0,0 +1,8 @@
+var idade = 70
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade > 69){
+        console.log('Voto opcional')
+    } else {
+        console.log('Voto obrigatório')
+        }
