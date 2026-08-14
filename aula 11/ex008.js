d94f4a8fe1db9656b0e5b2a226e6d5fdd79d5a02
juliano-magalhaes <aj.magalhaes1996@gmail.com>
2026-08-13 21:47:39 -0300

    ex008

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você utrapassou o limite de velocidade')
+}
+console.log('Dirija sempre usando cinto de segurança!')
