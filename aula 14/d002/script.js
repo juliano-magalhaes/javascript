86982b0da57bec202ba40099776bb0500a64d549
juliano-magalhaes <aj.magalhaes1996@gmail.com>
2026-08-19 16:29:06 -0300

    aula 14

--- aula 14/d002/script.js
+++ aula 14/d002/script.js
@@ -3,19 +3,20 @@ function contar() {
     var nc = Number(document.getElementById('nc').value)
     var nf = Number(document.getElementById('nf').value)
     var resu = document.getElementById('resu')
-    resu.innerHTML = 'Contando... '
+    resu.innerHTML = 'Contando... <br>'
     
     if(ni == 0 || nc == 0 || nf ==0){
         alert('ERRO impossivel contar')
     } else {
         if (ni < nf) {
             for(var c = ni; c <= nf; c += nc){
-                resu.innerHTML += `${c} `
+                resu.innerHTML += `${c} \u{1f449}`
             }
         } else {
             for(var c = ni; c >= nf; c -= nc) {
-                resu.innerHTML += `${c} `
+                resu.innerHTML += `${c} \u{1f449} `
             }
         }
     }
+    resu.innerHTML += '<br> Acabou \u{1f3c1}'
 }
